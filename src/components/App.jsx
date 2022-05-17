93c7abb47520947cb8ff426c5dc2595746d0b851
Eden Castillo <ecastilloasport@gmail.com>
2022-05-17 13:20:06 -0500

    [Add] Improved note component to be reusable. Add map function to App component to better map the added array.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
         <Heading />
-        <Note />
+        {notes.map(note => (
+          <Note
+            id={note.key} 
+            key={note.key}
+            title={note.title}
+            content={note.content}
+          />
+        ))}
         <Footer />
     </div>
   );
